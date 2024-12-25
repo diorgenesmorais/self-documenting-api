97aae678cacf6c8312fead148ac2efea000eec0a
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2024-12-25 15:17:30 -0300

    feat: add environment variable usage

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,32 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { routes } from "./routes";
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const envSchema = z.object({
+    PORT: z
+        .string()
+        .transform(Number)
+        .refine((val) => val > 0 && val < 65536, {
+            message: "The PORT must be a number between 1 and 65535"
+        }),
+    NODE_ENV: z.enum(["development", "production", "test"]),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+    console.error(
+        "Validation error in environment variables:",
+        parsedEnv.error.format()
+    );
+    process.exit(1);
+}
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
@@ -22,11 +48,11 @@ app.register(fastifySwagger, {
             version: "1.0.0",
         },
         servers: [
-            { url: "http://localhost:3000", description: "Local server" },
+            { url: `http://localhost:${PORT}`, description: "Local server" },
         ],
         tags: [{ name: "users", description: "User related endpoints" }],
     },
-    transform: jsonSchemaTransform
+    transform: jsonSchemaTransform,
 });
 app.register(fastifySwaggerUi, {
     routePrefix: "/docs",
@@ -34,6 +60,6 @@ app.register(fastifySwaggerUi, {
 
 app.register(routes);
 
-app.listen({ port: 3000 }).then(() => {
-    console.log("Server is running on port 3000");
+app.listen({ port: PORT }).then(() => {
+    console.log(`Server is running on port: ${PORT}`);
 });
