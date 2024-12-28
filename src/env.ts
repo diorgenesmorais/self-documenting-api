d159948aaf089ae561fc56568afc743e4f915065
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-12-28 06:43:33 -0300

    feat: add environment variable validation and refactor server configuration

---
+++ src/env.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z
+        .string()
+        .transform(Number)
+        .refine((val) => val > 0 && val < 65536, {
+            message: "The PORT must be a number between 1 and 65535"
+        }),
+    NODE_ENV: z.enum(["development", "production", "test"]),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+    console.error(
+        "Validation error in environment variables:",
+        _env.error.format()
+    );
+    process.exit(1);
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -9,29 +9,9 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { routes } from "./routes";
-import { z } from 'zod';
+import { env } from "./env";
 
-const PORT = Number(process.env.PORT) || 3000;
-
-const envSchema = z.object({
-    PORT: z
-        .string()
-        .transform(Number)
-        .refine((val) => val > 0 && val < 65536, {
-            message: "The PORT must be a number between 1 and 65535"
-        }),
-    NODE_ENV: z.enum(["development", "production", "test"]),
-});
-
-const parsedEnv = envSchema.safeParse(process.env);
-
-if (!parsedEnv.success) {
-    console.error(
-        "Validation error in environment variables:",
-        parsedEnv.error.format()
-    );
-    process.exit(1);
-}
+const PORT = env.PORT as number;
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
